277542f679d60b9f0474e10947795a23b989def3
Andrey Makarov <mailbox@anmcarrow.me>
2017-06-11 18:28:47 +0500

    Fix deploy script

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -3,7 +3,7 @@
 # Because actual integraton of Travis and Openshoft Cloud v3 is sucks,
 # we just use webhook. Amen.
 
-apt-get -qy install curl &&
+# apt-get -qy install curl &&
 curl -X POST https://api.starter-us-west-2.openshift.com/oapi/v1/namespaces/test-wp/buildconfigs/wp2/webhooks/192f97bca334e98e/generic
 
 exit 0
